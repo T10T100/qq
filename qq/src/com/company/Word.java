517df5d87c4f671192dcbe7317be2088e9e38e5a
T10T100 <piececakeof@mail.ru>
2015-11-05 13:06:13 +0300

    Performance to the the Word class

--- qq/src/com/company/Word.java
+++ qq/src/com/company/Word.java
@@ -5,46 +5,94 @@ package com.company;
  */
 public class Word {
     private String name;
-    private String word;
+    private String template;
 
-    public Word (String name, String word)
+    public Word (String name, String template)
     {
-        this.name = name;
-        this.word = word;
+        this.name = new String(name);
+        this.template = new String(template);
     }
 
-    public Word (String word)
+    public Word (String template)
     {
         this.name = new String("[name]");
-        this.word = word;
+        this.template = new String(template);
     }
 
     public Word ()
     {
         this.name = new String("[name]");
-        this.word = new String("[word]");
+        this.template = new String("[word]");
     }
 
 
+    public void setName (String name)
+    {
+        this.name = new String(name);
+    }
+    public  void setTemplate (String template)
+    {
+        this.template = new String(template);
+    }
+    public void set (String name, String template)
+    {
+        this.name = new String(name);
+        this.template = new String(template);
+    }
 
-    public boolean containsName (DefaultParseKeys key)
+
+    public String getName()
+    {
+        return name;
+    }
+    public String getTemplate ()
+    {
+        return this.template;
+    }
+    public String[] get ()
+    {
+        String[] output = {this.name, this.template};
+        return output;
+    }
+
+    public boolean containsKeysInName (DefaultParseKeys... keys)
+    {
+        for (DefaultParseKeys key : keys) {
+            if (this.name.contains(key.get()) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean containsKeyInName (DefaultParseKeys key)
     {
         return this.name.contains(key.get());
     }
 
-    public  boolean containsWord (DefaultParseKeys key)
+    public  boolean containsKeyInTemplate (DefaultParseKeys key)
     {
-        return this.word.contains(key.get());
+        return this.template.contains(key.get());
     }
 
-    public boolean equalsName (String name)
+    public boolean containsKeysInTemplate (DefaultParseKeys... keys)
+    {
+        for (DefaultParseKeys key : keys) {
+            if (this.template.contains(key.get()) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equalsNameTo (String name)
     {
         return this.name.contentEquals(name);
     }
 
-    public boolean equalsWord (String name)
+    public boolean equalsTemplateTo (String name)
     {
-        return this.word.contentEquals(name);
+        return this.template.contentEquals(name);
     }
 
 
