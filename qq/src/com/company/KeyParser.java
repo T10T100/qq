442e59a59aca5cc68966adf8b88507183378e0b9
T10T100 <piececakeof@mail.ru>
2015-11-09 16:01:55 +0300

    gonna use Word class into the main program

--- qq/src/com/company/KeyParser.java
+++ qq/src/com/company/KeyParser.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Afony on 01.11.2015.
@@ -21,7 +23,8 @@ public class KeyParser {
     {
         String garbage = "[garbage]";
         ArrayList<String> output = new ArrayList<>();
-        Word Pattern;
+        Word pattern = new Word();
+        boolean setted = false;
 
         ArrayList<Word> templates = this.getArrayOfWords(in, '<', '>');
         if (templates.isEmpty() == false) {
@@ -51,12 +54,21 @@ public class KeyParser {
 
         comparator.setUp(keys);
         String arg[];
+        setted = false;
         for (Word w : patterns) {
             arg = w.getFromValueOnce('\"', '\"');
             w.setValue(arg[1]);
             w.addArg(arg[0]);
             w.removeSpacesFromWord();
+            if (w.name.contentEquals("out")) {
+                pattern = w;
+                setted = true;
+            } else {
+                setted = false;
+            }
         }
+
+
 
         output.add("Keys : ");
         for (Word w : keys) {
@@ -74,6 +86,13 @@ public class KeyParser {
         output.add(new String(garbage));
         output.add("\nSystem : ");
         output.add(comparator.getSystemInfo());
+        output.add("\nRegular : \n");
+        if (setted == true) {
+            Pattern p = Pattern.compile(pattern.value);
+            Matcher matcher = p.matcher("0123456789abcd");
+            output.add("0123456789abcd\n");
+            output.add(matcher.toMatchResult().toString());
+        }
         return output;
     }
 
