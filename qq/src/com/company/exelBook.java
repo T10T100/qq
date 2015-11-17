d8a80f31fe5359098dfc7efcd6e965aa669a7638
T10T100 <piececakeof@mail.ru>
2015-11-17 13:53:45 +0300

    V0.243(stable - no date in paths comparison)

---
+++ qq/src/com/company/exelBook.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javafx.application.Application;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Locale;
+
+/**
+ * Created by k on 17.11.2015.
+ */
+public class exelBook  {
+    private Path location;
+    private String name;
+
+    public exelBook (Path location, String name)
+    {
+
+    }
+}
